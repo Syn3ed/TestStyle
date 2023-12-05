82a23768927a4776bd8fdd66eabb59934bcf47f9
Syn3ed <lol-uuu@bk.ru>
2023-12-05 13:55:20 +0700

    modified:   src/Operator/RequestDescriptionForm.js

--- src/Operator/RequestDescriptionForm.js
+++ src/Operator/RequestDescriptionForm.js
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect, useState,useNavigate } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 
