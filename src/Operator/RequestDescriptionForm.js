c51ef03c8ed3e5829e8f7cf8bf9da3d793bb947c
Syn3ed <lol-uuu@bk.ru>
2023-12-05 14:35:11 +0700

    modified:   src/Operator/RequestDescriptionForm.css 	modified:   src/Operator/RequestDescriptionForm.js

--- src/Operator/RequestDescriptionForm.js
+++ src/Operator/RequestDescriptionForm.js
@@ -129,48 +129,55 @@ const RequestDescriptionForm = ({ request }) => {
 
     return (
         <div className="request-description-form">
-            <h2>Описание заявки</h2>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="username">Никенейм пользователя</label>
-                    <input type="text" id="username" name="username" value={request.username} readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="subject">Тема заявки</label>
-                    <input type="text" id="subject" name="subject" value={request.subject} readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="address">Адрес</label>
-                    <input type="text" id="address" name="address" value={request.address} readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="description">Описание</label>
-                    <input type="text" id="description" name="description" value={request.description} readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="dialog">Диалог с оператором</label>
-                    <textarea type="text" id="dialog" name="dialog" value={request.dialog} readOnly />
-                </div>
-                {renderButtons()}
-                <div>
-                    {dataArray.length > 0 ? (
-                        dataArray.map((med) => (
-                            <div className="request-item">
-                                <div className="request-id">{med.id}</div>
-                                <div className="request-subject">{med.UserRequestId}</div>
-                                <div>
-                                    <button type="button" onClick={() => handleShowPhoto(med.id)}>Показать фото</button>
-                                </div>
-                            </div>
-                        ))
-                    ) : (
-                        <div>Загрузка данных...</div>
-                    )}
-                </div>
-
-            </form>
+          <h2>Описание заявки</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">Никенейм пользователя</label>
+              <input type="text" id="username" name="username" value={request.username} readOnly />
+            </div>
+            <div className="form-group">
+              <label htmlFor="subject">Тема заявки</label>
+              <input type="text" id="subject" name="subject" value={request.subject} readOnly />
+            </div>
+            <div className="form-group">
+              <label htmlFor="address">Адрес</label>
+              <input type="text" id="address" name="address" value={request.address} readOnly />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Описание</label>
+              <textarea
+                id="description"
+                name="description"
+                value={request.description}
+                readOnly
+                className="auto-height-textarea"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="dialog">Диалог с оператором</label>
+              <textarea id="dialog" name="dialog" value={request.dialog} readOnly className="auto-height-textarea" />
+            </div>
+            {renderButtons()}
+            <div>
+              {dataArray.length > 0 ? (
+                dataArray.map((med) => (
+                  <div className="request-item" key={med.id}>
+                    <div className="request-id">{med.id}</div>
+                    <div className="request-subject">{med.UserRequestId}</div>
+                    <div>
+                      <button type="button" onClick={() => handleShowPhoto(med.id)}>
+                        Показать фото
+                      </button>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div>Загрузка данных...</div>
+              )}
+            </div>
+          </form>
         </div>
-    );
+      );
 
 };
 
