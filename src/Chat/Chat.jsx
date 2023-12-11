2903f092bd74c0717cd9402367dd283b1b269d70
Syn3ed <lol-uuu@bk.ru>
2023-12-11 12:06:32 +0700

    modified:   src/Chat/Chat.css 	modified:   src/Chat/Chat.jsx

--- src/Chat/Chat.jsx
+++ src/Chat/Chat.jsx
@@ -1,32 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Chat.css'; // Подключите свои стили
+import './Chat.css';
 
 const Chat = () => {
-  const [chatMessages, setChatMessages] = useState([]);
-
-  useEffect(() => {
-    const fetchChatMessages = async () => {
-      try {
-        const response = await axios.get('https://tg-server-0ckm.onrender.com/chat'); // Замените на ваш адрес эндпоинта
-        setChatMessages(response.data);
-      } catch (error) {
-        console.error('Ошибка при получении сообщений чата', error);
-      }
-    };
-
-    fetchChatMessages();
-  }, []);
-
-  return (
-    <div className="chat-container">
-      {chatMessages.map((message, index) => (
-        <div key={index} className={message.roleUser === 'User' ? 'User' : 'Operator'}>
-          {message.textMessage}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Chat;
+    const [chatMessages, setChatMessages] = useState([]);
+  
+    useEffect(() => {
+      const fetchChatMessages = async () => {
+        try {
+          const response = await axios.get('https://tg-server-0ckm.onrender.com/chat');
+          setChatMessages(response.data);
+        } catch (error) {
+          console.error('Ошибка при получении сообщений чата', error);
+        }
+      };
+  
+      fetchChatMessages();
+    }, []);
+  
+    return (
+      <div className="chat-container">
+        {chatMessages.map((message, index) => (
+          <div key={index} className={message.roleUser === 'User' ? 'User' : 'Operator'}>
+            <div className="message-header">{message.idUser}</div>
+            {message.textMessage}
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Chat;
