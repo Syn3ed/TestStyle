fdf7142855c44e5cb96f0f85fc40f995549359c6
Syn3ed <lol-uuu@bk.ru>
2023-12-24 23:21:23 +0700

    modified:   src/FormReq/RequestForm.jsx

--- src/FormReq/RequestForm.jsx
+++ src/FormReq/RequestForm.jsx
@@ -1,20 +1,22 @@
 import React, { useCallback, useState } from 'react';
 import './Style.css';
+
 const RequestForm = () => {
     const [address, setAddress] = useState('');
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
 
     const tg = window.Telegram.WebApp;
+
     const onSendData = useCallback(() => {
         const data = {
             address,
             category,
             description
-        }
+        };
         tg.sendData(JSON.stringify(data));
-        console.log(JSON.stringify(data))
-    }, [address, category, description,tg])
+        console.log(JSON.stringify(data));
+    }, [address, category, description, tg]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,6 +24,9 @@ const RequestForm = () => {
         console.log('Category:', category);
         console.log('Description:', description);
     };
+
+    // Проверка, все ли поля формы заполнены
+    const isFormValid = address.trim() !== '' && category.trim() !== '' && description.trim() !== '';
 
     return (
         <div className="request-description-form">
@@ -57,9 +62,10 @@ const RequestForm = () => {
                         onChange={(e) => setDescription(e.target.value)}
                         required
                     />
-                </div>
-
-                <button type="submit" onClick={onSendData}>Отправить заявку</button>
+                </div> 
+                <button type="submit" onClick={onSendData} disabled={!isFormValid}>
+                    Отправить заявку
+                </button>
             </form>
         </div>
     );
