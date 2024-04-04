61919af8639b098c4423c7233afe238310e79a9a
Syn3ed <lol-uuu@bk.ru>
2024-04-04 09:55:30 +0700

    modified:   src/New/Admin/SearchUser.js

--- src/New/Admin/SearchUser.js
+++ src/New/Admin/SearchUser.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import './AdminStyle.css';
 import { useNavigate } from 'react-router-dom';
 
 const SearchUser = () => {
